05bdfc5a206b66fa7fd58b6a7c7daac707cce7f2
Camilo Gálvez Vidal <c.galvezv@uniandes.edu.co>
2022-11-06 13:32:28 -0300

    Add Auth methods LogIn and SignUp implementations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Post, UseGuards, Request, Body} from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from "./auth/guards/local-auth.guard";
+import {AuthService} from "./auth/auth.service";
+import {User} from "@prisma/client";
+import {UserService} from "./user/user.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+      private readonly appService: AppService,
+      private authSrv: AuthService,
+      private userSrv: UserService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHealthCheck(): string {
+    return this.appService.getHealthCheck();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req: any) {
+    const { email } = req.body;
+    return this.authSrv.login(email);
+  }
+
+  @Post('auth/signup')
+  async signup(
+      @Body() postData: { email: string ; password: string; name?: string }
+  ): Promise<User> {
+    return this.userSrv.createUser(postData);
   }
 }
